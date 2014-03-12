013c4444ef13d212204cf535367caf98bb68b870
kirkbro <maek@itu.dk>
2014-03-12 16:34:22 +0100

    Segmenteret quadtree Quadtree er nu loadet som et array, hvor hvert element repræsenterer en Edge-gruppe.

--- Map/Model/Loader.java
+++ Map/Model/Loader.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class Loader extends KrakLoader
 {
 	public final Node[] ndata;
-	public final QuadTree[] edata;
+	public final QuadTree edata;
 
 	public Loader()
 	{
@@ -16,9 +16,7 @@ public class Loader extends KrakLoader
 		// Low Y:  6 049 914,43018
 		// High Y: 6 402 050,98297
 		ndata = new Node[700000];
-		edata = new QuadTree[edge.getGroupLength()];
-		for(int i = 0; i < edata.length; i++)
-			edata[i] = new QuadTree(new double[][] {{442254.35659, 6049914.43018}, {892658.21706, 6402050.98297}});
+		edata = new QuadTree(new double[][] {{442254.35659, 6049914.43018}, {892658.21706, 6402050.98297}});
 		
 		try {
 			System.out.println(new File(".").getCanonicalPath());
@@ -39,6 +37,19 @@ public class Loader extends KrakLoader
 	public void processEdge(EdgeData edge)
 	{
 		Node[] nodes = new Node[] {ndata[edge.FNODE], ndata[edge.TNODE]};
-		edata[edge.getGroup()].insert(new Edge(edge, nodes));
+		edata.insert(new Edge(edge, nodes));
+	}
+
+	public static void main(String[] args) throws IOException {
+		Loader mkp = new Loader();
+
+		String dir = "../data/";
+		mkp.load(dir + "kdv_node_unload.txt", dir + "kdv_unload.txt");
+		System.out.println(mkp.edata.count);
+		
+		ArrayList<Edge> edges = mkp.edata.queryRange(new double[][] {{442254, 6049914}, {443254, 6402050}});
+		for(Edge edge : edges) {
+			System.out.println(edge.toString());
+		}
 	}
 }

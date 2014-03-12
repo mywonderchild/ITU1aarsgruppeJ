158f3242c746116b1a9f6420aa91c4009e3998dc
Bjørn Friese <bearfrieze@gmail.com>
2014-03-12 11:53:36 +0100

    Tilføjelse af load timer og oprydning
    - Tilføjet load timer
        - Smart fordi man kan se hvornår den er færdig med at loade
        - Godt værktøj til at performance teste eventuelle optimeringer af
    data load
    - Fjernet udkommenterede koordinater da vi ikke længere har brug for
    dem som reference

--- Map/Main.java
+++ Map/Main.java
@@ -7,14 +7,21 @@ import Map.Model.*;
 public class Main
 {
 	public static void main(String[] args) {
+
+		// Set up view
 		Canvas canvas = new Canvas();
 		Window window = new Window(canvas);
+
+		// Set up model
+		long start = System.currentTimeMillis(); // Timer start
 		QuadTree qt = (new Loader()).edata;
+		long stop = System.currentTimeMillis(); // Timer stop
+		System.out.printf(
+			"Done loading data after %.2f seconds.\n",
+			(stop - start) / 1000.0
+		);
+
 		Translator translator = new Translator(canvas, qt);
 		translator.setLines(qt.getBounds());
-		// Low X:  442 254,35659
-		// High X: 892 658,21706
-		// Low Y:  6 049 914,43018
-		// High Y: 6 402 050,98297
 	}
 }

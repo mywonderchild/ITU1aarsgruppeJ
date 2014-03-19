50ff6af793c062b31bd6e3a84da43bb69a548e2c
Ansuddin <ansuddin7@gmail.com>
2014-03-19 17:21:17 +0100

    virker

--- Map/View/Window.java
+++ Map/View/Window.java
@@ -1,17 +1,37 @@
 package Map.View;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
 
 	private Canvas canvas;
+	private JPanel buttomPanel;
+	private JLabel label;
 
 	public Window(Canvas canvas) {
 		super();
 
-		// Add canvas
+		setMinimumSize(new Dimension(400, 300));
+		setPreferredSize(new Dimension(800,625));
+
+		// set canvas
 		this.canvas = canvas;
-		this.getContentPane().add(canvas);
+
+        // set buttomPanel
+		buttomPanel = new JPanel();
+		buttomPanel.setPreferredSize(new Dimension(800,25));
+		buttomPanel.setBackground(new Color(204,204,204));
+
+		// Set label
+		label = new JLabel();
+		label.setText("ITU1aarsgruppeJ");
+        label.setMaximumSize(new Dimension(800, 25));
+        buttomPanel.add(label);
+
+        // add canvas and buttomPanel
+		getContentPane().add(canvas, BorderLayout.CENTER);
+        getContentPane().add(buttomPanel, BorderLayout.PAGE_END);
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);

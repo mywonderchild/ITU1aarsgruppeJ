2caa6978316e09aa9db1bce0155ab28dd130a4d0
kirkbro <maek@itu.dk>
2014-03-19 13:43:34 +0100

    MouseHandler Flyttet listener til musehandling ud af viewet og ind i controlleren i klassen MouseHandler.

---
+++ Map/Controller/MouseHandler.java
@@ -0,0 +1,39 @@
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseHandler implements MouseListener, MouseMotionListener {
+	private Canvas canvas;
+
+	public MouseHandler(Canvas canvas) {
+		this.canvas = canvas;
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	public void mouseDragged(MouseEvent e) {
+
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		// INSERT CODE FOR LOCATING NEAREST ROAD HERE!
+	}
+}

92190642e36878ef95266ae64fbf96bb22d30d4a
Bjørn Friese <bearfrieze@gmail.com>
2014-04-14 15:58:03 +0200

    Ans og Bjørn har fixet sidebar for vildt

--- Map/View/Window.java
+++ Map/View/Window.java
@@ -11,39 +11,31 @@ import javax.swing.JFrame;
 import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ComponentListener;
+import java.awt.event.ComponentEvent;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLayeredPane;
 import javax.swing.BorderFactory;
 import javax.swing.border.EtchedBorder;
+
 import java.awt.Insets;
 
 public class Window extends JFrame {
 
 	public Canvas canvas;
-	public JPanel sidePanel, bottomPanel, innerPanel;
+	public JLayeredPane topPanel;
+	public JPanel bottomPanel, sidePanel, innerPanel;
 	public JLabel label;
 	public JToggleButton toggler;
 	
 	public Window(Canvas canvas) {
 		super();
 
+		this.canvas = canvas;
+
 		setMinimumSize(new Dimension(600, 400));
 		setLayout(new BorderLayout());
 
-		// Set bottomPanel and label
-		bottomPanel = new JPanel();
-		bottomPanel.setBackground(new Color(235,235,235));
-		label = new JLabel("ITU1aarsgruppeJ");
-		bottomPanel.add(label);
-
-		// Set sidePanel		
-		sidePanel = new JPanel();
-		sidePanel.setBackground(new Color(235,235,235));
-		sidePanel.setLayout(new BorderLayout());
-
-		// Set canvas and panel
-		this.canvas = canvas;
-		
 		// Set innerPanel, labels and textfields.
 		innerPanel = new JPanel(new MigLayout());
 		JLabel text = new JLabel("Enter directions: ");
@@ -58,18 +50,15 @@ public class Window extends JFrame {
 		innerPanel.add(toText, "wmax 150, wrap");
 		innerPanel.add(new JButton("Get route"),"skip, gapleft 65");
 		innerPanel.setVisible(false);
-		sidePanel.add(innerPanel, BorderLayout.CENTER);
-		
+
 		// Toggler
 		toggler = new JToggleButton(">");
-		// FIND A WAY TO SLIM DOWN THE WIDTH
 		toggler.setMargin(new Insets(0, 0, 0, 0));
 		toggler.setBackground(Color.LIGHT_GRAY);
-		sidePanel.add(toggler, BorderLayout.EAST);
 		toggler.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JToggleButton tBtn = (JToggleButton)e.getSource();
-				if(tBtn.isSelected()) {
+				if (tBtn.isSelected()) {
 					innerPanel.setVisible(true);
 					toggler.setText("<");
 					sidePanel.setBorder(BorderFactory.createEtchedBorder());
@@ -78,16 +67,50 @@ public class Window extends JFrame {
 					toggler.setText(">");
 					sidePanel.setBorder(null);
 				}
+				resize();
 			}
 		});
 
-		// Add canvas and bottomPanel
-		getContentPane().add(canvas, BorderLayout.CENTER);
-		getContentPane().add(bottomPanel, BorderLayout.SOUTH);
-		getContentPane().add(sidePanel, BorderLayout.WEST);
+		// SidePanel		
+		sidePanel = new JPanel();
+		sidePanel.setBackground(new Color(235,235,235));
+		sidePanel.setLayout(new BorderLayout());
+		sidePanel.add(innerPanel, BorderLayout.CENTER);
+		sidePanel.add(toggler, BorderLayout.EAST);
 
+		// TopPanel
+		topPanel = new JLayeredPane();
+		topPanel.add(canvas, new Integer(0));
+		topPanel.add(sidePanel, new Integer(1));
+
+		// Label
+		label = new JLabel("ITU1aarsgruppeJ");
+
+		// BottomPanel
+		bottomPanel = new JPanel();
+		bottomPanel.setBackground(new Color(235,235,235));
+		bottomPanel.add(label);
+
+		// Add topPanel and bottomPanel
+		getContentPane().add(topPanel, BorderLayout.CENTER);
+		getContentPane().add(bottomPanel, BorderLayout.SOUTH);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.pack();
+
+		// Set canvas size
+		topPanel.addComponentListener(new ComponentListener() {
+			{componentResized(null);}
+			public void componentResized(ComponentEvent e) {resize();}
+		    public void componentHidden(ComponentEvent e) {}
+		    public void componentMoved(ComponentEvent e) {}
+		    public void componentShown(ComponentEvent e) {}
+		});
+	}
+
+	private void resize() {
+		canvas.setBounds(0, 0, topPanel.getWidth(), topPanel.getHeight());
+		int width = toggler.isSelected() ? 200 : 50;
+		sidePanel.setBounds(0, 0, width, topPanel.getHeight());
 	}
 }

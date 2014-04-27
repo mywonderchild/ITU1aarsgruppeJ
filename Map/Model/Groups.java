78c839c23e1b4bde671541847301697a8f33bed6
Bjørn Friese <bearfrieze@gmail.com>
2014-04-27 15:55:10 +0200

    Finding edge group, color and width now takes ~constant time

--- Map/Model/Groups.java
+++ Map/Model/Groups.java
@@ -7,81 +7,65 @@ import Map.Model.Edge;
 public class Groups {
 
 	public final static int[][] GROUPS;
+	public final static int[] GROUPMAP;
+	public final static Color[] COLORMAP;
+	public final static int[] WIDTHMAP;
 
 	static {
-		// Groups
 		// We view "Motortrafik" and "Sekundærrute" as main roads
 		// We view "Markvej" as a path
 		// Road type 0 and 95 are undocumented but present – we added them to other
-		GROUPS = new int[7][];
-		GROUPS[0] = new int[]{1, 21, 31, 41}; // Highways
-		GROUPS[1] = new int[]{2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44}; // Main roads
-		GROUPS[2] = new int[]{8, 10, 28, 48}; // Paths
-		GROUPS[3] = new int[]{11}; // Pedestrian
-		GROUPS[4] = new int[]{80}; // Naval
-		GROUPS[5] = new int[]{81}; // Coastline
-		GROUPS[6] = new int[]{0, 5, 6, 25, 26, 35, 45, 46, 95, 99}; // Other
+		GROUPS = new int[][] {
+			{1, 21, 31, 41}, // Highways
+			{2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44}, // Main roads
+			{8, 10, 28, 48}, // Paths
+			{11}, // Pedestrian
+			{80}, // Naval
+			{81}, // Coastline
+			{0, 5, 6, 25, 26, 35, 45, 46, 95, 99} // Other
+		};
+
+		// Build groupmap
+		GROUPMAP = new int[100];
+		for (int i = 0; i < GROUPMAP.length; i++)
+			GROUPMAP[i] = -1;
+		for (int i = 0; i < GROUPS.length; i++)
+			for (int j = 0; j < GROUPS[i].length; j++)
+				GROUPMAP[GROUPS[i][j]] = i;
+
+		// Build colormap
+		COLORMAP = new Color[] {
+			Color.RED,
+			Color.BLUE,
+			Color.GREEN,
+			Color.MAGENTA,
+			Color.CYAN,
+			Color.BLACK,
+			Color.BLACK
+		};
+
+		// Build widthmap
+		WIDTHMAP = new int[] {3, 1, 1, 1, 1, 1, 1};
 	}
 
 	public static int getGroup(int type) throws RuntimeException {
-
-		// Determine road group
-		for (int i = 0; i < GROUPS.length; i++)
-			for (int id : GROUPS[i])
-				if (type == id)
-					return i;
-
-		throw new RuntimeException("Road group not found, type is: " + type);
+		int group = GROUPMAP[type];
+		if (group != -1)
+			return group;
+		else
+			throw new RuntimeException("Road group not found, type is: " + type);
 	}
 
-	public static Color getGroupColor(int group) throws RuntimeException {
-
-		// Return color
-		switch(group) {
-			case 0:
-				return Color.RED;
-			case 1:
-				return Color.BLUE;
-			case 2:
-				return Color.GREEN;
-			case 3:
-				return Color.MAGENTA;
-			case 4:
-				return Color.CYAN;
-			case 5:
-				return Color.BLACK;
-			case 6:
-				return Color.BLACK;
-			default:
-				throw new RuntimeException("Road group " + group + " is not recognized");
-		}
+	private static Color getGroupColor(int group) {
+		return COLORMAP[group];
 	}
 
 	public static Color getColor(Edge edge) throws RuntimeException {
 		return getGroupColor(getGroup(edge.TYPE));
 	}
 
-	public static int getGroupWidth(int group) throws RuntimeException {
-
-		// Return color
-		switch(group) {
-			case 0:
-				return 3;
-			case 1:
-				return 1;
-			case 2:
-				return 1;
-			case 3:
-				return 1;
-			case 4:
-				return 1;
-			case 5:
-				return 1;
-			case 6:
-				return 1;
-			default:
-				throw new RuntimeException("Road group " + group + " is not recognized");
-		}
+	private static int getGroupWidth(int group) {
+		return WIDTHMAP[group];
 	}
 
 	public static int getWidth(Edge edge) throws RuntimeException {
